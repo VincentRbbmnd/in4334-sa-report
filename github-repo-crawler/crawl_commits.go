c9ed5ed4db2dc5ac994a146f5139666b914aa4e7
wim_spaargaren@live.nl <wim_spaargaren@live.nl>
2017-09-25 10:46:39 +0200

    Now stops crawling commits if current is added and first commit is in db.

--- github-repo-crawler/crawl_commits.go
+++ github-repo-crawler/crawl_commits.go
@@ -30,10 +30,48 @@ type Author struct {
 
 func startCommitCrawling(repoID int64, repoName string) {
 	url := "https://api.github.com/repos/" + repoName + "/commits?per_page=100"
-	getCommitsOfRepo(repoID, url)
+	// fmt.Println("SHA OF FIRST COMMIT: ",
+	getCommitsOfRepo(repoID, url, isFirstCommitInDB(getSHAOfFirstCommit(repoName)))
 }
 
-func getCommitsOfRepo(repoID int64, apiUrl string) {
+func getSHAOfFirstCommit(repoName string) string {
+	resp := githubAPICall("https://api.github.com/repos/"+repoName+"/commits?per_page=100", "GET", nil)
+	link := resp.Header.Get("link")
+	if link != "" {
+		parsedLinkHeader := parseLinkHeader(resp.Header.Get("link"))
+		resp = githubAPICall(parsedLinkHeader.Last.URL, "GET", nil)
+	}
+	var res RawCommitData
+	err := json.NewDecoder(resp.Body).Decode(&res)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Println("Data could not be decoded into struct", err)
+	}
+	byteData, err := json.Marshal(res[len(res)-1])
+	if err != nil {
+		fmt.Println("Raw data could not be converted to bytes", err)
+	}
+	var commitData ImportantCommitData
+	err = json.Unmarshal(byteData, &commitData)
+	if err != nil {
+		fmt.Println("Raw repo data could not be decoded further into struct", err)
+	}
+
+	return commitData.SHA
+}
+
+func isFirstCommitInDB(sha string) bool {
+	var ctx context.Context
+	commit, err := commitDB.Get(ctx, sha)
+
+	// if commit already added return
+	if commit != nil || err == nil {
+		return true
+	}
+	return false
+}
+
+func getCommitsOfRepo(repoID int64, apiUrl string, firstCommitInDB bool) {
 	resp := githubAPICall(apiUrl, "GET", nil)
 
 	var res RawCommitData
@@ -60,23 +98,24 @@ func getCommitsOfRepo(repoID int64, apiUrl string) {
 			addUserToDB(committer, commitData.CommitterRaw)
 		}
 		//ADD COMMIT TO DB
-		addCommitToDB(commitData, byteData, repoID, int64(author.ID), int64(committer.ID))
+		wasAlreadyAdded := addCommitToDB(commitData, byteData, repoID, int64(author.ID), int64(committer.ID))
+		if wasAlreadyAdded && firstCommitInDB {
+			return
+		}
 	}
 	parsedLinkHeader := parseLinkHeader(resp.Header.Get("link"))
 	if parsedLinkHeader.Next != (Link{}) {
-		getCommitsOfRepo(repoID, parsedLinkHeader.Next.URL)
-	} else {
-
+		getCommitsOfRepo(repoID, parsedLinkHeader.Next.URL, firstCommitInDB)
 	}
 }
 
-func addCommitToDB(commitData ImportantCommitData, byteData []byte, repositoryID int64, authorID int64, committerID int64) {
+func addCommitToDB(commitData ImportantCommitData, byteData []byte, repositoryID int64, authorID int64, committerID int64) bool {
 	var ctx context.Context
 	commit, err := commitDB.Get(ctx, commitData.SHA)
 
 	// if commit already added return
 	if commit != nil || err == nil {
-		return
+		return true
 	}
 
 	var dbCommit models.Commit
@@ -97,4 +136,5 @@ func addCommitToDB(commitData ImportantCommitData, byteData []byte, repositoryID
 	if err != nil {
 		fmt.Println("commit not added to db", err)
 	}
+	return false
 }
