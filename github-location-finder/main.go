9a4b9e898c4b75c70d6a35dcc1a55e3ae66c4584
wim_spaargaren@live.nl <wim_spaargaren@live.nl>
2017-10-03 06:30:22 +0200

    Now fully working location finder

--- github-location-finder/main.go
+++ github-location-finder/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -63,14 +64,10 @@ func main() {
 	for 1 < 2 {
 		fmt.Println("repo: ", repos[counter].FullName)
 		users := userDB.ListNoLocationsForRepo(ctx, repos[counter].ProjectID)
-
 		if len(users) == 0 {
-
+			counter++
 		}
-		for i, user := range users {
-			fmt.Println("Length users for repo: ", repos[counter].FullName, len(users))
-			fmt.Println("user login: ", user.User.Login, "Number: ", i)
-			fmt.Println(user.ID, "test", user.GithubUserID)
+		for _, user := range users {
 			location := getUserLocation(user.Login)
 			processUserLocation(location, user)
 		}
@@ -82,6 +79,7 @@ func processUserLocation(location string, user *UserContainer) {
 
 	fmt.Println("Location: ", location)
 	if location == "" {
+		fmt.Println("No location found: ", location)
 		user.LocationChecked = true
 		err := userDB.Update(ctx, &user.User)
 		if err != nil {
@@ -91,17 +89,21 @@ func processUserLocation(location string, user *UserContainer) {
 		var ctx context.Context
 		locationFromDB, err := locationDB.GetByLocationString(ctx, location)
 		if err != nil {
-			googleLocation := getLocationGoogleForAddress(location)
-			fmt.Println("google loc: ", googleLocation)
-			locationID, err := locationDB.Add(ctx, googleLocation.Lat, googleLocation.Lng, user.GithubUserID, location)
-			if err != nil {
-				panic(err)
+			googleLocation, err := getLocationGoogleForAddress(location)
+			if err == nil {
+				fmt.Println("google loc: ", googleLocation)
+				locationID, err := locationDB.Add(ctx, googleLocation.Lat, googleLocation.Lng, user.User.GithubUserID, location)
+				if err != nil {
+					panic(err)
+				}
+				user.User.LocationID = locationID
 			}
-			user.LocationID = locationID
 		} else {
-			user.LocationID = locationFromDB.ID
+			fmt.Println("Location already found in db: ", locationFromDB.ID)
+			user.User.LocationID = locationFromDB.ID
 		}
-		user.LocationChecked = true
+
+		user.User.LocationChecked = true
 		err = userDB.Update(ctx, &user.User)
 		if err != nil {
 			panic(err)
@@ -152,31 +154,31 @@ func githubAPICall(url string, method string, payload *interface{}) *http.Respon
 	return resp
 }
 
-func getLocationGoogleForAddress(address string) LocationGoogle {
-	// var LocationGoogle LocationGoogle
+func getLocationGoogleForAddress(address string) (LocationGoogle, error) {
+	var locationGoogle LocationGoogle
 
 	url := "https://maps.googleapis.com/maps/api/geocode/json?address=" + strings.Replace(address, " ", "", -1) + "&key=AIzaSyDpy6APeHM3X1JVqdOyuNkZqOS242e8ij8"
-	fmt.Println("API CALL : ", url)
+	fmt.Println("GOOGLE PLACES API CALL : ", url)
 	resp := googleAPICall(url)
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return locationGoogle, err
 	}
 
 	defer resp.Body.Close()
 	var googleAddress GoogleAddresses
 	err = json.Unmarshal(body, &googleAddress)
 	if err != nil {
-		panic(err)
+		return locationGoogle, err
 	}
 	if googleAddress.ErrorMessage != "" {
-		panic(googleAddress.ErrorMessage)
+		panic("PLACES API RATE LIMIT THIS SHOULD NOT HAPPEN" + googleAddress.ErrorMessage)
 	}
 	if len(googleAddress.Results) == 0 {
-		panic("AAAH")
+		return locationGoogle, errors.New("AAAH")
 	}
 	fmt.Println("Google address: ", googleAddress)
-	return googleAddress.Results[0].Geometry.LocationGoogle
+	return googleAddress.Results[0].Geometry.LocationGoogle, nil
 }
 
 func googleAPICall(url string) *http.Response {
