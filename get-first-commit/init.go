2f6cbf2c4db7430747c3d6dc6f047a76d76e93d5
Vincent Robbemond <vincentrobbemond@gmail.com>
2017-10-15 20:23:32 +0200

    Add get first commit pack

---
+++ get-first-commit/init.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	ghmodels "github.com/VincentRbbmnd/in4334-sa-report/github-repo-crawler/models"
+
+	"github.com/jinzhu/gorm"
+)
+
+func initDatabase(logMode bool) {
+	var err error
+	db, err = InitDatabase()
+	if err != nil {
+		panic(err)
+	}
+	db.LogMode(logMode)
+	db.Exec("CREATE EXTENSION IF NOT EXISTS \"postgis\";")
+	db.AutoMigrate(&ghmodels.Repo{}, &ghmodels.Commit{})
+
+	repoDB = ghmodels.NewRepoDB(db)
+	commitDB = ghmodels.NewCommitDB(db)
+
+	db.DB().SetMaxOpenConns(50)
+}
+
+func InitDatabase() (*gorm.DB, error) {
+	host := os.Getenv("PG_HOST")
+	databaseName := os.Getenv("PG_DBNAME")
+	user := os.Getenv("PG_USERNAME")
+	pass := os.Getenv("PG_PASSWORD")
+	if host == "" {
+		host = "86.87.235.82"
+	}
+	if databaseName == "" {
+		databaseName = "github_crawler"
+	}
+	if user == "" {
+		user = "postgres"
+	}
+	if pass == "" {
+		pass = "postgres"
+	}
+
+	// Connect to DB
+	dbHost := flag.String("host", host, "Defaults to localhost")
+	dbName := flag.String("db", databaseName, "Defaults to hoppa_dev")
+	dbUser := flag.String("user", user, "Defaults to postgres")
+	dbPass := flag.String("pass", pass, "Defaults to empty")
+	flag.Parse()
+
+	fmt.Println("Configuration:")
+	fmt.Println("DB HOST: " + *dbHost)
+	fmt.Println("DB Name: " + *dbName)
+	fmt.Println("DB User: " + *dbUser)
+
+	url := fmt.Sprintf("dbname=%s user=%s password=%s sslmode=disable port=%d host=%s", *dbName, *dbUser, *dbPass, 8082, *dbHost)
+
+	return gorm.Open("postgres", url)
+}
---
+++ get-first-commit/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	. "github.com/VincentRbbmnd/in4334-sa-report/github-repo-crawler/models"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/lib/pq"
+)
+
+var db *gorm.DB
+var userDB *UserDB
+var locationDB *LocationDB
+var repoDB *RepoDB
+var commitDB *CommitDB
+
+func main() {
+	initDatabase(false)
+	var ctx context.Context
+	repos, err := repoDB.List(ctx)
+	if err != nil {
+		panic(err)
+	}
+	for _, repo := range repos {
+		fmt.Println("repo: ", repo.FullName)
+		commit, err := commitDB.GetFirstCommitByRepoID(ctx, repo.ProjectID)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("first commit sha: ", commit.SHA)
+		fmt.Println("first commit date: ", commit.CommitDate)
+	}
+}
